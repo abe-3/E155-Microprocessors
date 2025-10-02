b7ca25e69964d67869bc4404e004d0d84f492038
abe-3 <147130798+abe-3@users.noreply.github.com>
2025-10-02 15:03:58 -0700

    Delete lab4/mcu/SEGGER_proj directory

--- lab4/mcu/SEGGER_proj/Output/Debug/Obj/lab4_ar/SEGGER_THUMB_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/lab4_ar/SEGGER_THUMB_Startup.o: \
- C:\Users\abrah\OneDrive\Documents\E155-Microprocessors\lab4\mcu\SEGGER_proj\SEGGER_THUMB_Startup.s
--- lab4/mcu/SEGGER_proj/Output/Debug/Obj/lab4_ar/stm32l432xx_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/lab4_ar/stm32l432xx_Vectors.o: \
- C:\Users\abrah\OneDrive\Documents\E155-Microprocessors\lab4\mcu\SEGGER_proj\STM32L4xx\Source\stm32l432xx_Vectors.s
